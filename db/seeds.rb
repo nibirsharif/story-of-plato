8925d72d3e643fb91a51a56f6aab77afc8595b1d
Shariful Nibir <nibirsharif@gmail.com>
2018-09-02 14:06:43 +0800

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+organizations = Organization.create([{ name: 'Oranage Org.' }, { name: 'Apple Inc.' }])
+users = User.create([{ name: 'Shariful', email: 'shariful@example.com', password: 'abc123', organization_id: organizations.first.id },
+                     { name: 'Nibir', email: 'nibir@example.com', password: 'abc123', organization_id: organizations.last.id }])
+
+project = Project.create(name: 'Project A', organization_id: organizations.first.id)
+project.users << users.first
+
+board = Board.create(name: 'DevX', project_id: project.id)
+
+cards = Card.create([{ title: 'Finance report', description: 'Generate finance report file', color: '#0000FF', board_id: board.id, user_id: users.first.id },
+                     { title: 'Refactor report', description: 'Refactor finance report file', color: '#0000FF', board_id: board.id, user_id: users.first.id }])
+
+task_list = TaskList.create(color: '#FF3333', card_id: cards.first.id, completed: false)
+
+Task.create([{ state: 1, description: 'Generate report schema', task_list_id: task_list.id },
+             { state: 0, description: 'Create excel file', task_list_id: task_list.id }])
+
+cards.last.card_referrers << cards.first
